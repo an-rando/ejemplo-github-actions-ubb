cbf880828f21fcc38f033aea22a8e0c9d59bdf9d
an-rando <sebastian.espinoza1701@alumnos.ubiobio.cl>
2025-05-08 18:38:00 -0400

    prueba de workflow con dependencia y error en test

--- src/index.test.js
+++ src/index.test.js
@@ -1,10 +1,10 @@
-const request = require('supertest');
-const app = require('./index');
+const request = require("supertest");
+const app = require("./index");
 
-describe('GET /', () => {
-    it('debe responder con el archivo index.html', async () => {
-        const res = await request(app).get('/');
-        expect(res.statusCode).toBe(200);
-        expect(res.headers['content-type']).toMatch(/html/);
-    });
+describe("GET /", () => {
+  it("debe responder con el archivo index.html", async () => {
+    const res = await request(app).get("/");
+    expect(res.statusCode).toBe(201);
+    expect(res.headers["content-type"]).toMatch(/html/);
+  });
 });
