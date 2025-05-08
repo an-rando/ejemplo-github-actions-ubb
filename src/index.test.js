fe180275f9caf77d170080f9c03ccd97acb74bc3
an-rando <sebastian.espinoza1701@alumnos.ubiobio.cl>
2025-05-08 18:43:56 -0400

    ajusta el código de prueba para que verifique el estado 200 en lugar de 201 y añade tiempo de espera en las conexiones VPN y SSH en el flujo de trabajo de despliegue

--- src/index.test.js
+++ src/index.test.js
@@ -4,7 +4,7 @@ const app = require("./index");
 describe("GET /", () => {
   it("debe responder con el archivo index.html", async () => {
     const res = await request(app).get("/");
-    expect(res.statusCode).toBe(201);
+    expect(res.statusCode).toBe(200);
     expect(res.headers["content-type"]).toMatch(/html/);
   });
 });
